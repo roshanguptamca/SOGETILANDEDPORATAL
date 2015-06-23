5d10ebc54e27632502d2bcd2b167d46156888be2
Rushabhshah999 <shah.rushabh.shah@gmail.com>
2015-06-23 16:13:50 +0200

    Added for Employee search

---
+++ LandedPortalCore/src/main/java/com/sogeti/model/CustomUser.java
@@ -0,0 +1,35 @@
+package com.sogeti.model;
+
+public class CustomUser {
+
+	private String emp_id;
+	private String name;
+	private String address;
+	private String contactno;
+	
+	public String getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(String emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContactno() {
+		return contactno;
+	}
+	public void setContactno(String contactno) {
+		this.contactno = contactno;
+	}
+
+}
