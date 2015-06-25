3caadaf2ed103274505e1fbbeeaa45f593ac5aa4
Sachin <sachin4pundalik@gmail.com>
2015-06-25 15:54:55 +0200

    Code commited for add knowledge base module

---
+++ LandedPortalUsers/src/main/java/com/sogeti/VO/CustomerVO.java
@@ -0,0 +1,21 @@
+package com.sogeti.VO;
+
+public class CustomerVO {
+	
+	private Integer cid;
+	private String customerName;
+	
+	public Integer getCid() {
+		return cid;
+	}
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+}
